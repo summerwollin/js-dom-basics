3ac9d0c7d0fa4ffc9b7ea5422328c1dd7dce7be7
Summer Wollin <summer.wollin@gmail.com>
2016-02-15 14:27:20 -0700

    querySelector() and querySelectorAll()

--- main.js
+++ main.js
@@ -13,3 +13,24 @@ console.log(allListItems);
 for (var i = 0; i < allListItems.length; i++) {
   console.log(allListItems[i]);
 }
+
+var evenElements = document.getElementsByClassName('even');
+console.log(evenElements)
+
+var oddElements = document.getElementsByClassName('odd');
+console.log(oddElements)
+
+var firstEvenElement = document.getElementsByClassName('even')[0];
+console.log(firstEvenElement)
+
+var firstOddElement = document.getElementsByClassName('odd')[0];
+console.log(firstOddElement)
+
+var main = document.querySelector('#main');
+console.log(main);
+
+var firstOddElement = document.querySelector('.odd');
+console.log(firstOddElement);
+
+var oddAndEvenElements = document.querySelectorAll('.odd, .even');
+console.log(oddAndEvenElements);
