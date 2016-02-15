f7392def72737ce6ef1b9c2a1ec098c125ad193f
Summer Wollin <summer.wollin@gmail.com>
2016-02-15 14:17:51 -0700

    Include index.js, getElementsById, getElementsByTagName

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("sanity check!")
+var firstParagraph = document.getElementById('main');
+console.log(firstParagraph);
+
+var getAllElements = document.getElementsByTagName('ul');
+console.log(getAllElements)
+
+var getSingleElement = document.getElementsByTagName('ul')[0];
+console.log(getSingleElement)
+
+allListItems = document.getElementsByTagName('li');
+console.log(allListItems);
+
+for (var i = 0; i < allListItems.length; i++) {
+  console.log(allListItems[i]);
+}
